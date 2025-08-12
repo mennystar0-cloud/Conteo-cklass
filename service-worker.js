7f2047515cbc12acdbf470bbce1ad7d9879fc39d
mennystar0-cloud <mennystar0@gmail.com>
2025-08-12 08:16:42 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE = 'conteo-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (ASSETS.includes(url.pathname) || url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return resp;
+      }))
+    );
+  }
+});
