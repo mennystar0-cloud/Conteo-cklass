010ae38b96205d7065102419373b7db83eba7fdc
mennystar0-cloud <mennystar0@gmail.com>
2025-08-13 17:58:03 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,32 @@
-const CACHE = 'conteo-pwa-v1';
+const CACHE = "cklass-conteo-v1";
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  'https://cdn.tailwindcss.com'
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "https://cdn.tailwindcss.com",
+  "https://unpkg.com/@ericblade/quagga2/dist/quagga.min.js"
 ];
 
-self.addEventListener('install', e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-  if (ASSETS.includes(url.pathname) || url.origin === location.origin) {
-    e.respondWith(
-      caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+self.addEventListener("fetch", (e) => {
+  const { request } = e;
+  if (request.method !== "GET") return;
+  e.respondWith(
+    caches.match(request).then(cached => cached ||
+      fetch(request).then(resp => {
         const copy = resp.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
+        caches.open(CACHE).then(c => c.put(request, copy));
         return resp;
-      }))
-    );
-  }
+      }).catch(() => cached)
+    )
+  );
 });
